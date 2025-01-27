3f505b33950cf7fa251693f6110de4db6db51b5e
Skey30 <150328180+Skey30@users.noreply.github.com>
2025-01-27 15:36:05 +0100

    peut être

--- IHM_AJOUT/ADD_PARCOURS.xaml.cs
+++ IHM_AJOUT/ADD_PARCOURS.xaml.cs
@@ -26,6 +26,8 @@ namespace IHM_BASE {
             BDD = new C_BDD();
             InitializeComponent();
 
+            LB_Parcours.ItemsSource = BDD.Get_All_Parcours();
+            LB_Parcours.DisplayMemberPath = nameof(C_PARCOURS.nomParcours);
         }
 
 
@@ -60,8 +62,8 @@ namespace IHM_BASE {
                     imgPathParcours = Path
                 };
                 BDD.Create_Parcours(Parcours);
-                MessageBox.Show($"Le parcours '{Parcours.nomParcours}' a été ajouté avec succès.","Succès",MessageBoxButton.OK,MessageBoxImage.Information);
-                Close();
+                ////MessageBox.Show($"Le parcours '{Parcours.nomParcours}' a été ajouté avec succès.","Succès",MessageBoxButton.OK,MessageBoxImage.Information);
+                ////Close();
             } catch(Exception ex) {
                 MessageBox.Show($"Une erreur est survenue : {ex.Message}\n{ex.StackTrace}","Erreur",MessageBoxButton.OK,MessageBoxImage.Error);
             }
@@ -85,8 +87,8 @@ namespace IHM_BASE {
                 return;
             }
 
-            BDD.Delete_Espece(Parcours_Select.idEspece);
-            var List_Parcours = BDD.Get_All_Especes();
+            BDD.Delete_Parcours(Parcours_Select.idParcours);
+            var List_Parcours = BDD.Get_All_Parcours();
             LB_Parcours.ItemsSource = List_Parcours;
             LB_Parcours.SelectedIndex = 0;
         }

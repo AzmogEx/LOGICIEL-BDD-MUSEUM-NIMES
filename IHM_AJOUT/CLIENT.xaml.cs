3e9ce520a10819fe21d47bdd96e8c7988cae18d1
Adam MARZUK <a.marzuk@lycee-cci-gard.fr>
2025-02-05 13:40:42 +0100

    retrait du test color.xaml

--- IHM_AJOUT/CLIENT.xaml.cs
+++ IHM_AJOUT/CLIENT.xaml.cs
@@ -230,7 +230,7 @@ namespace IHM_BASE {
         }
 
         private void Btn_Admin_Click(object sender,RoutedEventArgs e) {
-            LOGIN mainMenu = new();
+            Menu mainMenu = new();
             mainMenu.Show();
             Close();
         }

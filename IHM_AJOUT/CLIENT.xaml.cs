959934e2f1dc90e8dd0980c293e00d3368d49f89
Adam MARZUK <a.marzuk@lycee-cci-gard.fr>
2025-02-10 13:43:42 +0100

    changement bouton admin

--- IHM_AJOUT/CLIENT.xaml.cs
+++ IHM_AJOUT/CLIENT.xaml.cs
@@ -231,7 +231,7 @@ namespace IHM_BASE {
         }
 
         private void Btn_Admin_Click(object sender,RoutedEventArgs e) {
-            LOGIN mainMenu = new();
+            Menu mainMenu = new();
             mainMenu.Show();
             Close();
         }

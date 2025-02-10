435dcd04afc245e0da787961c9d43a0a4ca8f5df
Adam MARZUK <a.marzuk@lycee-cci-gard.fr>
2025-02-10 14:31:31 +0100

    bloquage des touches

--- LIB_BDD/KeyboardHook.cs
+++ LIB_BDD/KeyboardHook.cs
@@ -1,36 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace LIB_BDD {
-    using System;
-    using System.Runtime.InteropServices;
-
     public static class KeyboardHook {
-        [DllImport("user32.dll",SetLastError = true)]
-        private static extern int RegisterHotKey(IntPtr hWnd,int id,uint fsModifiers,uint vk);
-
-        [DllImport("user32.dll",SetLastError = true)]
-        private static extern bool UnregisterHotKey(IntPtr hWnd,int id);
-
-        private const uint MOD_ALT = 0x0001;
-        private const uint MOD_CTRL = 0x0002;
-        private const uint MOD_WIN = 0x0008;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
         private const int VK_TAB = 0x09;
         private const int VK_F4 = 0x73;
+        private const int VK_ESCAPE = 0x1B;
+        private const int VK_LALT = 0xA4;
+        private const int VK_RALT = 0xA5;
 
-        private const int HOTKEY_ID = 9000;
+        private static IntPtr _hookID = IntPtr.Zero;
+        private static LowLevelKeyboardProc _proc = HookCallback;
 
-        public static void DisableSystemShortcuts(IntPtr windowHandle) {
-            RegisterHotKey(windowHandle,HOTKEY_ID,MOD_ALT,VK_TAB);  // Désactive Alt + Tab
-            RegisterHotKey(windowHandle,HOTKEY_ID + 1,MOD_ALT,VK_F4); // Désactive Alt + F4
+        public static void Start() {
+            _hookID = SetHook(_proc);
         }
 
-        public static void ReleaseHotKeys(IntPtr windowHandle) {
-            UnregisterHotKey(windowHandle,HOTKEY_ID);
-            UnregisterHotKey(windowHandle,HOTKEY_ID + 1);
+        public static void Stop() {
+            UnhookWindowsHookEx(_hookID);
         }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+            using(var curProcess = Process.GetCurrentProcess())
+            using(var curModule = curProcess.MainModule) {
+                return SetWindowsHookEx(WH_KEYBOARD_LL,proc,
+                    GetModuleHandle(curModule.ModuleName),0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode,IntPtr wParam,IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode,IntPtr wParam,IntPtr lParam) {
+            if(nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                // Bloquer les combinaisons interdites
+                if(IsKeyBlocked(vkCode)) {
+                    return (IntPtr)1; // Bloque l'événement clavier
+                }
+            }
+
+            return CallNextHookEx(_hookID,nCode,wParam,lParam);
+        }
+
+        private static bool IsKeyBlocked(int vkCode) {
+            // Vérifie si Alt est enfoncé et une des touches bloquées
+            bool isAltPressed = (GetAsyncKeyState(VK_LALT) < 0 || GetAsyncKeyState(VK_RALT) < 0);
+            return (isAltPressed && (vkCode == VK_TAB || vkCode == VK_F4)) || vkCode == VK_ESCAPE;
+        }
+
+        [DllImport("user32.dll",CharSet = CharSet.Auto,SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,LowLevelKeyboardProc lpfn,
+            IntPtr hMod,uint dwThreadId);
+
+        [DllImport("user32.dll",CharSet = CharSet.Auto,SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll",CharSet = CharSet.Auto)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk,int nCode,
+            IntPtr wParam,IntPtr lParam);
+
+        [DllImport("kernel32.dll",CharSet = CharSet.Auto,SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
     }
 }
